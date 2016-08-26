217e547ab72162f97c9e77dc0261eded0fb4e75d
Georg Echterling <georg@echterling.de>
2016-08-26 14:51:25 +0200

    Added try_opt_or! and try_opt_or_else!

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Self-Ref [![Build Status](https://travis-ci.org/Mixthos/try_or.svg?branch=master)](https://travis-ci.org/Mixthos/try_or)
 
-Contains the macros `try_or!` and `try_or_else!`, which are to `try!` what `unwrap_or` and `unwrap_or_else` are to `unwrap()`.
+Contains the macros `try_or!`, `try_or_else!`, `try_opt_or!` and
+`try_opt_or_else!`. These are helper macros for unwrapping a `Result` or an
+`Option` while returning early for `Err` and `None` values. The semantics
+are similar to `unwrap_or` and `unwrap_or_else`.
+
+If you want a `try_opt!` macro, there's already another crate (`try_opt`)
+for that.
 
 ## [Documentation](http://mixthos.github.com/try_or)
 
@@ -27,5 +33,13 @@ fn main() {
 	// test try_or_else!
 	assert_eq!({ || try_or_else!("1".parse::<u32>(), |_| 5) } (), 1);
 	assert_eq!({ || try_or_else!("a".parse::<u32>(), |_| 5) } (), 5);
+	
+	// test try_opt_or!
+	assert_eq!({ || try_opt_or!(Some(1), 2) } (), 1);
+	assert_eq!({ || try_opt_or!(None, 2) } (), 2);
+	
+	// test try_or_else!
+	assert_eq!({ || try_opt_or_else!(Some(1), || 2) } (), 1);
+	assert_eq!({ || try_opt_or_else!(None, || 2) } (), 2);
 }
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
-//! Contains the macros `try_or!` and `try_or_else!`, which are to
-//! `try!` what `unwrap_or` and `unwrap_or_else` are to `unwrap()`.
+//! Contains the macros `try_or!`, `try_or_else!`, `try_opt_or!` and
+//! `try_opt_or_else!`. These are helper macros for unwrapping a `Result` or an
+//! `Option` while returning early for `Err` and `None` values. The semantics
+//! are similar to `unwrap_or` and `unwrap_or_else`.
+//!
+//! If you want a `try_opt!` macro, there's already another crate (`try_opt`)
+//! for that.
 
 #![no_std]
 
@@ -29,6 +34,41 @@ macro_rules! try_or_else {
 }
 
 
+/*/// Unwraps an `Option`, returning `None` if the option is `None`.
+#[macro_export]
+macro_rules! try_opt {
+	( $expr:expr ) => {{
+		match { $expr } {
+			Some(val) => val,
+			None => return None,
+		}
+	}}
+}*/
+
+/// Unwraps an `Option`, returning the second parameter if the option is `None`.
+#[macro_export]
+macro_rules! try_opt_or {
+	( $expr:expr , $or:expr ) => {{
+		match { $expr } {
+			Some(res) => res,
+			None => return { $or },
+		}
+	}}
+}
+
+/// Unwraps an `Option`. If the result is `None`, calls the function `$or_fn`
+/// and returns its result.
+#[macro_export]
+macro_rules! try_opt_or_else {
+	( $expr:expr , $or_fn:expr ) => {{
+		match { $expr } {
+			Some(res) => res,
+			None => return { $or_fn }(),
+		}
+	}}
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -40,5 +80,13 @@ mod tests {
 		// test try_or_else!
 		assert_eq!({ || try_or_else!("1".parse::<u32>(), |_| 5) } (), 1);
 		assert_eq!({ || try_or_else!("a".parse::<u32>(), |_| 5) } (), 5);
+		
+		// test try_opt_or!
+		assert_eq!({ || try_opt_or!(Some(1), 2) } (), 1);
+		assert_eq!({ || try_opt_or!(None, 2) } (), 2);
+		
+		// test try_or_else!
+		assert_eq!({ || try_opt_or_else!(Some(1), || 2) } (), 1);
+		assert_eq!({ || try_opt_or_else!(None, || 2) } (), 2);
     }
 }
